3cb7573b2604f00e21373cc53a883b8d14dcf42c
2022wb86809 <2022wb86809@wilp.bits-pilani.ac.in>
2026-09-22 22:56:13 +0530

    Modified source code for CI testing

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -4,10 +4,10 @@ package com.bits;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello Harini-2022wb86809 in DevOps Lab!");
     }
 }
